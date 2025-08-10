fc704234e85574a956e0b2011a3458b20c9dd79d
Arpan Vala <valaarpan05@gmail.com>
2025-08-10 20:46:10 +0530

    Defined Routres in App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import { assets } from './assets/assets'
+import {Routes, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
 
 const App = () => {
   return (
-    <h1 class="text-3xl text-center font-light text-gray-9 flex items-center justify-center h-screen gap-10 ">
-  Welcome to Atom <img src={assets.logo} alt="logo" width={60} height={60} />
-  </h1>
+   <>
+
+  <Routes>
+    <Route path = "/" element={<Login/>}/>
+    <Route index element={<Feed/>}/>
+    {/* for list of prople messages  */}
+    <Route path="messages" element={<Messages/>}/>
+    {/* for individual message thread */}
+    <Route path="meassages/:userId" element={<Messages/>}/>
+
+    <Route path="connections" element={<Connections/>}/>
+    <Route path="discover" element={<Discover/>}/>
+
+    {/* for user profile */}
+    <Route path="profile" element={<Profile/>}/>
+    {/* for individual profile on platform */}
+    <Route path="profile/:profileId" element={<Profile/>}/>
+
+    <Route path="create-post" element={<CreatePost/>}/>
+
+
+    
+  </Routes>
+   </>
   )
 }
 
