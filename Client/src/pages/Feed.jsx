3e90107902da202137d1608f906f0ef1672816ed
Arpan Vala <valaarpan05@gmail.com>
2025-08-29 13:44:08 +0530

    ad card added in feed

--- Client/src/pages/Feed.jsx
+++ Client/src/pages/Feed.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets';
+import { assets, dummyPostsData } from '../assets/assets';
 import Loading from '../components/Loading';
 import StoryCards from '../components/StoryCards';
 import PostCard from '../components/PostCard';
+import { Megaphone } from 'lucide-react';
+import {useNavigate} from 'react-router-dom';
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
 
   //fech posts
   const fetchFeeds = async() => {
@@ -23,7 +27,7 @@ const Feed = () => {
   return !loading ? (
     <>
 
-    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex flex-col md:flex-row items-start md:justify-center xl:gap-8 '>
+    <div className='border h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start md:justify-center xl:gap-8 '>
 
       {/* stories and posts */}
       <div className=' '>
@@ -37,10 +41,21 @@ const Feed = () => {
       </div>
 
       {/* messages  */}
-      <div className='border'> 
+      <div className='max-xl:hidden sticky top-0'> 
+        <div className='max-w-xs bg-white text-xs p-4 group rounded-md flex flex-col gap-2 shadow hover:shadow-xl cursor-pointer relative'>
+        <a href="https://goalsetter-arpanvala.netlify.app/" target='_blank'>
+              <h3 className='text-slate-600 inline-flex gap-1 items-center font-semibold'>AD <Megaphone size={18} /></h3>
+              <div className='overflow-hidden rounded-md'>
+                <img src={assets.sponsored_img} alt="ad image" className=' w-full h-full object-cover group-hover:scale-110 transition duration-500' />
+              </div>
+            
+              <p className='text-slate-900'>GoalSetter - Arpan Vala</p>
+              <p className='text-slate-600'>Achieve your goals with personalized categories and easy tracking.</p>
+          </a>
+        </div>
+
         <div>
         Recent messages
-
         </div>
       </div>
     </div>
