c42170d296043d61d0ecac7b93dcd48d35aa8477
Arpan Vala <valaarpan05@gmail.com>
2025-09-05 19:48:43 +0530

    auth added

---
+++ Server/middleware/auth.js
@@ -0,0 +1,12 @@
+export default  protect = async (req, res, next) => {
+    try {
+        const {userId} = await req.auth();
+        if(!userId){
+            return res.status(401).json({success:false,message:'Not authorized'});
+        }
+        next();
+        
+    } catch (error) {
+        return res.status(401).json({success:false,message: error.message});
+    }
+}
