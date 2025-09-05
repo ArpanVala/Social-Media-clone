8a74543f7bc4af5974f11f1d98cecb6b303a71a0
Arpan Vala <valaarpan05@gmail.com>
2025-09-05 15:10:46 +0530

    Error fixed : serve , User imort in index.js, User model

--- Server/models/User.js
+++ Server/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
-        _id: {type:String, unique:true, required:true},
+        _id: {type:String,  required:true},
         email:{ type:String, required:true, unique:true },
         full_name:{ type:String, required:true },
         username:{ type:String, required:true, unique:true },
